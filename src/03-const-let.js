06d9a67d087b3b4254370c6074a7c5ce1ff1a53c
jordi Olmedillas <jordi.olmedillas@gmail.com>
2021-02-05 17:30:15 +0100

    03- Nuevas Formas de Declarar Variables

--- src/03-const-let.js
+++ src/03-const-let.js
@@ -8,3 +8,29 @@ let pais = 'Mejico';
 // let pais = 'España';
 
 console.log(`nombre ${pais}`);
+
+// -----------------------------------------------
+// * dentro de una función ni var ni let ni const son accesibles dentro de una function   
+function saluda() {
+    var nombre2 = 'john';
+    let apellido = 'Perez';
+    // return (`${nombre2} - ${apellido}`);
+    return (nombre2 + apellido);
+}
+console.log(saluda());
+
+// console.log(nombre2);
+// console.log(apellido);
+
+// -----------------------------------------------
+// * Let y const tienen un scope de tipo bloque
+var edad = 18;
+if (edad >= 18) {
+    var esAdulto = true;
+    let esAdulto2 = true;
+    const esAdulto3 = true;
+}
+console.log(esAdulto);
+// console.log(esAdulto2);
+// console.log(esAdulto3);
+// -----------------------------------------------
