9c129fcf58758679a380ee122f5fcff3eba6c76f
jordi Olmedillas <jordi.olmedillas@gmail.com>
2021-02-05 17:39:36 +0100

    template strings

---
+++ output/04-templateStrings.js
@@ -0,0 +1,4 @@
+const nombre = 'Carlos';
+const edad = 25;
+const pais = 'Méjico';
+console.log(`La persona es ${nombre} tiene ${edad} años y es de ${pais}`);
---
+++ src/04-templateStrings.js
@@ -0,0 +1,5 @@
+const nombre = 'Carlos';
+const edad = 25;
+const pais = 'Méjico';
+
+console.log(`La persona es ${nombre} tiene ${edad} años y es de ${pais}`);
