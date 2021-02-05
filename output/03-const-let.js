6601bc672d3a55ad9a9d42aaeb1f84f1a039f9d1
jordi Olmedillas <jordi.olmedillas@gmail.com>
2021-02-05 17:28:42 +0100

    03- Nuevas Formas de Declarar Variables

--- output/03-const-let.js
+++ output/03-const-let.js
@@ -1,10 +1,33 @@
-"use strict";
-
 // * Con var se puede redeclarar variables
 var nombre = 'jordi';
 var nombre = 'Pepe';
-console.log("nombre ".concat(nombre)); // ! ES6 con let no permite redeclarar variables
+console.log(`nombre ${nombre}`); // ! ES6 con let no permite redeclarar variables
 
-var pais = 'Mejico'; // let pais = 'España';
+let pais = 'Mejico'; // let pais = 'España';
 
-console.log("nombre ".concat(pais));
+console.log(`nombre ${pais}`); // -----------------------------------------------
+// * dentro de una función ni var ni let ni const son accesibles dentro de una function   
+
+function saluda() {
+  var nombre2 = 'john';
+  let apellido = 'Perez'; // return (`${nombre2} - ${apellido}`);
+
+  return nombre2 + apellido;
+}
+
+console.log(saluda()); // console.log(nombre2);
+// console.log(apellido);
+// -----------------------------------------------
+// * Let y const tienen un scope de tipo bloque
+
+var edad = 18;
+
+if (edad >= 18) {
+  var esAdulto = true;
+  let esAdulto2 = true;
+  const esAdulto3 = true;
+}
+
+console.log(esAdulto); // console.log(esAdulto2);
+// console.log(esAdulto3);
+// -----------------------------------------------
